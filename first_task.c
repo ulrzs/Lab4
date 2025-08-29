9c21fa54ed55c88647054aa88e9d5603cd2bc257
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-29 17:27:58 +0200

    Update first_task.c

--- first_task.c
+++ first_task.c
@@ -1,13 +1,27 @@
 /* Task description:
 
+Write functions that receive a real number parameter, and return:
+
+    cube() – its third power,
+    absolute() – its absolute value (there is a fabs() library function but do not use it this time)! 
+
+Write a program that tabulates the values of a, a3, |a| and sin(a) using 4 decimal digits after the decimal point from a = −1 to +1, stepping by 0.1! Include math.h to use sin.
 
 */
-
-
 #include <stdio.h>
 
-int main(){
+???cube()//<-- Function header, input parameters and output type in this form oType NAME(iType1 iName1,iType2 iName2,...)
+{//here starts what to do
+
+//reurn with the result of the funcion (if any)
+}//that closes the function
+
+/*Write the other functions here:*/
 
 
-  return 0;
+int main() //main is a function!
+{
+//call the functions in a loop to print the results in the required manner
+
+  return 0; //it returns an integer (as promised in the header) which is 0 for now!
 }
