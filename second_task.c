805e26df3adec817332888b7da6ae43b465d3d6d
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-29 17:30:57 +0200

    Update second_task.c

--- second_task.c
+++ second_task.c
@@ -1,5 +1,11 @@
 /* Task description:
+First part: Stammering (or stuttering) is a speech disorder when syllables (usually the first one in the words) are unintentionally repeated. 
+Look at the following ahartihifihicihiahal stahammehereher cohodehe (artificial stammerer code)
+for this you are not required to use your own functions
 
+Second part:Write a function to distinguish vowels from other letters (the function receives a character, and returns whether it is a vovel or not). 
+Modify the program above to use this function in main().
+For now we do not use the function many times, but we could! Please note that the program is much more readable now!
 
 */
 
