09807dd84caaa82a968acaad413e10bd35bce6cb
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-29 17:40:19 +0200

    Update fifth_task.c

--- fifth_task.c
+++ fifth_task.c
@@ -1,5 +1,5 @@
 /* Task description:
-
+Rest a little, or go to the homepage and solve some practice tasks...
 
 */
 
@@ -7,7 +7,13 @@
 #include <stdio.h>
 
 int main(){
-
-
+  int awake=0;
+  char str[51];
+  while(!awake){
+    printf("sleeping...");
+    scanf("%s",str); //if you know why it is dangerous to do, you are on a right way in programming!
+    if(!strcmp(str,"Breakfast!"))
+      awake=1;
+  }
   return 0;
 }
