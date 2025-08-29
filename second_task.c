543030d300578a6abffbe796c274b2b5ac14921a
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-29 17:32:04 +0200

    Update second_task.c

--- second_task.c
+++ second_task.c
@@ -7,6 +7,13 @@ Second part:Write a function to distinguish vowels from other letters (the funct
 Modify the program above to use this function in main().
 For now we do not use the function many times, but we could! Please note that the program is much more readable now!
 
+Third part: How to make it work for words starting with a capital? Like, for the word „Apart” „Ahapahart” should be printed. 
+To accomplish this the program must be able to identify uppercase vowels, too. The original character should be printed before h, and the lowercase counterpart after it. 
+You can use your existing vowel() function for deciding but always pass the lowercase letter to it.
+Hint:
+There are character handling library functions in #include <ctype.h>. One of them is tolower(). The tolower(c) expression is the lowercase counterpart of c if it is an uppercase letter, 
+for any other character it returns the original character.
+
 */
 
 
