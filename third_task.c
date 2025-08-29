633ce951754365c4496f0cba9f60ceb808886360
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-29 17:34:33 +0200

    Update third_task.c

--- third_task.c
+++ third_task.c
@@ -1,5 +1,11 @@
 /* Task description:
+A twin prime is a prime number that is either 2 less or 2 more than another prime number - for example, either member of the twin prime pair (41, 43). 
+In other words, a twin prime is a prime that has a prime gap of two.
 
+The first couple of twin primes are (3, 5), (5, 7) and (11, 13). Write a C program to find the 60th twin prime! 
+Extend the program to check whether the number between the twin primes has any digit equal to 5. 
+Avoid code duplication, use functions wherever reasonable! Follow the top-down design methodology.
+Top-down: when solving programming problems by breaking them down into smaller and smaller sub-problems until each one is trivial or known
 
 */
 
