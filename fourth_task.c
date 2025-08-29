741578a76dc2418e483afadd8356acf76e3b2220
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-29 17:35:41 +0200

    Update fourth_task.c

--- fourth_task.c
+++ fourth_task.c
@@ -1,5 +1,13 @@
 /* Task description:
+Create a simple menu controlled program! The program stores a number with an initial value of n = 1. Then the program should print the actual value of n and the menu below. After the user selects a menu item (scanf()) the program should do whatever the user requested and print the value of n and the menu! These must be repeated as long as the user chooses anything but the Exit menu option! Use switch() and post-testing do ... while() loop.
 
+printf("0. Restore the initial value (n = 1)\n"
+       "1. Add 1\n"
+       "2. Negate the number (+/-)\n"
+       "3. Multiple by 2\n"
+       "9. Exit\n");
+
+Realize each operation in a (tiny) function that receives the value of n as a parameter and returns the modified value! Your main() can only call these functions to change the value of n.
 
 */
 
